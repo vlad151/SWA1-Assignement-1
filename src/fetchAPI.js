0e4fefaf2d497fa8025e2f34d2052028a7f7d2ec
vlad151 <lazar.vlad151@gmail.com>
2023-09-13 20:26:40 +0200

    got the basic forecast going on

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -1,5 +1,5 @@
 import { populateHourlyForecast, populateLatestMeasurements, populateLastDayStats } from './populateData.js';
-
+import dayjs from 'dayjs';
 document.addEventListener('DOMContentLoaded', () => {
     const citySelect = document.getElementById('city-select');
   
@@ -8,21 +8,66 @@ document.addEventListener('DOMContentLoaded', () => {
       fetchWeatherData(selectedCity);
     });
   });
-  
-  function fetchWeatherData(city) {
-    fetch(`https://your-server-url.com/weather?city=${city}`)
+  const headers = { 'Content-Type': 'application/json', Accept: 'application/json'}
+ export function fetchWeatherData(city) {
+    fetch(`http://localhost:8080/data/${city}`,{method:"GET", body: JSON.stringify(), headers})
       .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
+        if (response.ok) {
+         return response.json();
+        }else{
+         throw new Error('Network response was not ok ' + response.statusText);}
       })
-      .then(data => {
-        // Now data contains the weather data
-        // You can use this data to populate your HTML elements
-      })
-      .catch(error => {
-        console.error('There has been a problem with your fetch operation:', error);
-      });
+      // .then(data => {
+      //   // Now data contains the weather data
+      //   // You can use this data to populate your HTML elements
+      //   // console.log("data", data)
+      //   return data
+      // })
+      // .catch(error => {
+      //   console.error('There has been a problem with your fetch operation:', error);
+      // });
   }
+const ul= document.getElementById("forecastSection")
+const list = document.createDocumentFragment();
+
+
+  export function forecastData(city) {
+    fetch(`http://localhost:8080/forecast/${city}`,{method:"GET", body: JSON.stringify(), headers})
+      .then(response => {
+        if (response.ok) {
+         return response.json(); 
+        }else{
+        // console.log(response.json(), "response")
+       return Promise.reject(response.statusText)
+  }})
   
+  .then(data=>{
+    console.log("data", data)
+   let events= data
+   events.map(function(event){
+    let li = document.createElement('li');
+        let type = document.createElement('span');
+        let from =document.createElement('span')
+        let to=document.createElement('span')
+        let time =document.createElement('span')
+
+        type.innerHTML = `${event.type}`;
+        from.innerHTML=`${event.from} ${event.unit}`
+        to.innerHTML=`${event.to} ${event.unit}`
+      
+        time.innerHTML=`${dayjs(event.time).hour()}:00`
+      li.appendChild(time)
+        li.appendChild(type)
+        li.appendChild(from)
+        li.appendChild(to)
+ 
+        
+        
+        list.appendChild(li)
+        ul.appendChild(list)
+
+       
+   })
+  return data
+  })
+}
--- src/init.js
+++ src/init.js
@@ -1,2 +1,26 @@
 import "../src/styles.css"
-console.log("test")
+import { fetchWeatherData, forecastData } from "./fetchAPI";
+import dayjs from "dayjs";
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Find the element with the class "card"
+//     var cardElement = document.querySelector(".card");
+
+//     // Add a click event listener to the card element
+//     cardElement.addEventListener("click", function() {
+//         // This function will be executed when the card is clicked
+//     //   console.log(forecastData("Horsens"))
+//     console.log(forecastData("Horsens"))
+//     });
+// })
+
+let cityForecastData=forecastData("Horsens")
+
+const startTime= dayjs().startOf("D")
+const endTime=dayjs().endOf("D")
+console.log(startTime, "start TIme")
+console.log("end", endTime)
+console.log(forecastData("Horsens"), "forecast data")
+
+// const filteredData= cityForecastData.filter(data=>data.time.isAfter(startTime))
+// console.log("filtered data", filteredData)
